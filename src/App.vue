aad6c6e25c8b3e379d878d161f8af65b5eb74fef
Igor Luiz <igor@devstacks.com.br>
2017-10-07 23:53:16 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <hr>
+    <IGHeader />
+    <IGArticle />
+    <IGFooter />
+  </div>
+</template>
+
+<script>
+import IGHeader from './components/Header.vue'
+import IGArticle from './components/Article.vue'
+
+export default {
+  name: 'app',
+  components: {
+    IGHeader,
+    IGArticle,
+    IGFooter: () => import(/* webpackChunkName: "footer" */ './components/Footer.vue')
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  text-align: center;
+}
+</style>
